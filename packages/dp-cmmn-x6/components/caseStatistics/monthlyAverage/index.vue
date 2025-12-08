6885276202938714c76bff5d23e5debbc6a6d929
Sean Tsang <9394wing@gmail.com>
2025-12-08 11:35:28 +0800

    Remove commented-out axisPointer configuration in monthlyAverage component

--- packages/dp-cmmn-x6/components/caseStatistics/monthlyAverage/index.vue
+++ packages/dp-cmmn-x6/components/caseStatistics/monthlyAverage/index.vue
@@ -63,12 +63,12 @@ function handleDelete() {
 const option = {
   tooltip: {
     trigger: 'axis',
-    axisPointer: {
-      type: 'cross',
-      crossStyle: {
-        color: '#999'
-      }
-    }
+    // axisPointer: {
+    //   type: 'cross',
+    //   crossStyle: {
+    //     color: '#999'
+    //   }
+    // }
   },
   xAxis: [
     {
