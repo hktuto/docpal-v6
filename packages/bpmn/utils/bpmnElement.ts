9ad7daae1020ca3cf797658e4b3ff81172c1ee70
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-12-11 16:56:50 +0800

    [UPDATE] refactor workflow log Task

--- packages/bpmn/utils/bpmnElement.ts
+++ packages/bpmn/utils/bpmnElement.ts
@@ -497,7 +497,7 @@ export const bpmnElement: BpmnElement = {
             color = '#7B61FF'
             break
           case '${createAuditLogDelegate}':
-            icon = '/bpmn/icons/case.svg'
+            icon = '/bpmn/icons/log.svg'
             type = 'Audit Log Task'
             color = '#7B61FF'
             break
@@ -1074,7 +1074,7 @@ export const bpmnElement: BpmnElement = {
         })
       },
       {
-        icon: 'bpmn:case',
+        icon: 'bpmn:log',
         label: 'Audit Log',
         group: 'case',
         order: 0,
