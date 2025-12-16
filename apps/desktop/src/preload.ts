46ce7e16ccd6c2eec920d29598acdbd3c4aae2c8
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-12-16 16:32:34 +0800

    [UPDATE] update electron notifier

--- apps/desktop/src/preload.ts
+++ apps/desktop/src/preload.ts
@@ -1,9 +1,12 @@
-import { contextBridge, ipcRenderer } from 'electron'
+import { app, contextBridge, ipcRenderer, Notification } from 'electron'
 //@ts-ignore
-const notifier = require('node-notifier');
+const notifier = require('node-notifier')
 
 //@ts-ignore
 window.ipcRenderer = require('electron').ipcRenderer
+
+// 消息標簽頭
+app.setAppUserModelId('Docpal')
 
 contextBridge.exposeInMainWorld('shell', {
   open: () => ipcRenderer.send('shell:open'),
@@ -25,18 +28,30 @@ window.addEventListener('dragTagToWindow', (event: any) => {
 window.addEventListener('sendMessage', (event: any) => {
   console.log('Message notification', event)
 
+  // noti
+  notifier.notify({
+    title: 'My notification',
+    message: 'Hello, there!',
+    icon: '/icon.png'
+  })
+
+  // el
+  const options = {
+    title: 'Electron Notification',
+    body: '',
+    silent: true,
+    icon: '/icon.png'
+  }
+  new Notification(options).show()
+
+
   const checkFocused = () => new Promise<boolean>((resolve) => {
     ipcRenderer.once('reply-focused', (_e, focused: boolean) => resolve(focused))
     ipcRenderer.send('check-focused')
   })
-
   checkFocused().then((focused) => {
     if (focused) {
       return
     }
-    notifier.notify({
-      title: 'My notification',
-      message: 'Hello, there!'
-    });
   })
 })
