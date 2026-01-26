e0d76524b07f715492463439c86783e1711a00f5
Sean Tsang <9394wing@gmail.com>
2026-01-26 08:50:39 +0800

    fix max listener

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+require('events').EventEmitter.defaultMaxListeners = 50;
 
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
