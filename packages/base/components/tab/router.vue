9a2d3b2da1a5eaef145d34a7e982b53e48f95afd
Sean Tsang <9394wing@gmail.com>
2026-01-30 10:40:43 +0800

    add more log

--- packages/base/components/tab/router.vue
+++ packages/base/components/tab/router.vue
@@ -310,11 +310,8 @@ onUnmounted(() => {
       <Icon :name="tab.icon" />
     </Teleport>
     <Teleport v-if="tab.label" defer :to="`#${isFullscreen ? 'fullscreen-' : ''}tab-header-${tab.parent}-${tab.id} > .label`">
-      <div class="label">
-        {{ t(tab.label) }}
-      </div>
+      <div class="label">{{ t(tab.label) }} {{ tab.initized }}</div>
     </Teleport>
-
     <template v-if="tab.initized">
       <Transition>
         <Suspense>
