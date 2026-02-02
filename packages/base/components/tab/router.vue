97acb8d4b8ecd2efb6297cd3be65ba18b3770b2d
Sean Tsang <9394wing@gmail.com>
2026-02-02 16:36:49 +0800

    test component

--- packages/base/components/tab/router.vue
+++ packages/base/components/tab/router.vue
@@ -314,34 +314,31 @@ onUnmounted(() => {
     </Teleport>
     <template v-if="tab.initized">
       renderComponent_0: {{ renderComponent }} tab : {{ tab }}<br />
+      <component v-if="renderComponent" :is="tab.component" :tab="tab" v-bind="tab.props" />
       <Transition>
         <Suspense>
-          <template v-if="!tab.handleError">
-            <NuxtErrorBoundary ref="errorBoundary" @error="handleErr">
-              renderComponent: {{ renderComponent }} tab : {{ tab }}<br />
-              <component v-if="renderComponent" :is="tab.component" :tab="tab" v-bind="tab.props" />
-              <template #error="{ error, clearError }">
-                <div class="errorBoundaryContainer">
-                  <div class="messageContainer">
-                    <h3 class="errorTitle">ERROR : {{ $t(tab.label) }}</h3>
-                    <pre>
-                       {{ error }}
-                    </pre>
-                    <pre>
-                      {{ tab }}
-                    </pre>
-                    <el-button :icon="Refresh" @click="clearError">
-                      {{ $t('common_refresh') }}
-                    </el-button>
-                  </div>
-                </div>
-              </template>
-            </NuxtErrorBoundary>
-          </template>
-          <template v-else>
-            tab.component : {{ tab.component }}<br />
+          <NuxtErrorBoundary ref="errorBoundary" @error="handleErr">
+            renderComponent: {{ renderComponent }} tab : {{ tab }}<br />
             <component v-if="renderComponent" :is="tab.component" :tab="tab" v-bind="tab.props" />
-          </template>
+            <template #error="{ error, clearError }">
+              <div class="errorBoundaryContainer">
+                <div class="messageContainer">
+                  <h3 class="errorTitle">ERROR : {{ $t(tab.label) }}</h3>
+                  <pre>
+                       {{ error }}
+                    </pre
+                  >
+                  <pre>
+                      {{ tab }}
+                    </pre
+                  >
+                  <el-button :icon="Refresh" @click="clearError">
+                    {{ $t('common_refresh') }}
+                  </el-button>
+                </div>
+              </div>
+            </template>
+          </NuxtErrorBoundary>
           <template #fallback>
             <LoadingBgInline />
           </template>
