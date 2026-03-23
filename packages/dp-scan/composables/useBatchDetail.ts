f7871fdb598f5ce9cf0cb846a5649660c632a490
Sean Tsang <9394wing@gmail.com>
2026-03-23 12:04:35 +0800

    add log

--- packages/dp-scan/composables/useBatchDetail.ts
+++ packages/dp-scan/composables/useBatchDetail.ts
@@ -627,6 +627,7 @@ export const useBatchDetail = (batchId: string) => {
       // TODO : Select Next Document, and reload page
       // TODO:　calculate selected document index
       const index = batchDetail.value.documents.findIndex((b) => b.id === currentSelectedDoc.value.id)
+      cosnole.log("try to get next index", index, currentSelectedDoc.value)
       if (index !== -1 ) {
         if (index === batchDetail.value.documents.length - 1) {
           // is last page
