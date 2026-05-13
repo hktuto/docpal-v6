0d92e5043a078681c4fe76948d2f77684056f51e
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2026-05-13 18:13:53 +0800

    [UPDATE] update new Workflow

--- pages/client-new-workflow/components/workflow/allTask.vue
+++ pages/client-new-workflow/components/workflow/allTask.vue
@@ -17,7 +17,7 @@ const { tableConfig, tableEvent, tableRef, query, reload, cleanSelectedRows } = 
   api: async (pageParams: any) => {
     const data = (await $api.get(`/oniflow/api/v1/task/overview/available/${userId}`).then((r: any) => r.data)) as any[]
     // 只保留 waiting 狀態的數據
-    let list = data.filter((item: any) => item.status === 'waiting')
+    let list = data.task.filter((item: any) => item.status === 'waiting')
 
     if (extraParams.value.definition_id !== '') {
       list = list.filter((item: any) => item.definition_id === extraParams.value.definition_id)
