fe75ef518b9150df427451e91846919f5dca64e3
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2026-07-08 21:18:21 +0800

    [UPDATE] update Quotation Approval

--- packages/workflow/components/global/formWidget/quotationApproval/historicalPriceDialog.vue
+++ packages/workflow/components/global/formWidget/quotationApproval/historicalPriceDialog.vue
@@ -10,26 +10,41 @@ const searchData = ref<any>({
   date_range: [],
   type: ''
 })
-const tableData = ref<any[]>([])
+const index = ref<number>()
 const selectList = ref<any[]>([])
 const { tableConfig, tableEvent, tableRef, reload, cleanSelectedRows } = useVxeTable({
   id: 'a-user-table',
   api: async (pageParams: any) => {
-    return tableData.value
+    return handleSearch()
   },
   columns: [
-    { field: 'type', title: '類型 Type', fixed: 'left', type: 'checkbox' },
-    { field: 'number', title: '編號 Number' },
-    { field: 'item', title: '項 Item' },
+    { field: 'poNumber', title: '編號 Number', fixed: 'left', type: 'checkbox', width: 200 },
+    { field: 'type', title: '類型 Type', width: 130 },
+    { field: 'item', title: '項 Item', width: 300 },
     { field: 'moq', title: '起訂量 MOQ' },
-    { field: 'quantity', title: '數量 Quantity' },
+    { field: 'quantity', title: '數量 Quantity', width: 200 },
     { field: 'cost', title: '成本 Cost' },
     { field: 'currency', title: '貨幣 Currency' },
-    { field: 'effective_date', title: '生效日期 Effective Date' },
-    { field: 'note_to_vendor', title: '供應商注意事項 Note to Vendor' }
+    { field: 'poCustomer', title: '客戶 Customer', width: 400 },
+    {
+      field: 'creationDate',
+      title: '生效日期 Effective Date',
+      formatter({ cellValue }: any) {
+        return formatDate(cellValue)
+      },
+      width: 200
+    },
+    { field: 'noteToVendor', title: '供應商注意事項 Note to Vendor', width: 300 }
   ],
   selectChangeHander: (selectedRows: any[]) => {
     selectList.value = [...selectedRows]
+  },
+  optionalConfig: {
+    scrollX: {
+      enabled: true,
+      gt: 40
+    },
+    autoResize: true
   }
 })
 
@@ -40,21 +55,171 @@ function open(item: any) {
     part_number: item.part_number || '',
     series: item.series || ''
   }
+  index.value = item.index
 }
 
 async function handleSearch() {
-  console.log()
+  // tableData.value = [{}]
+  // cell API Get data
+
+  // test Data
+  const data = [
+    {
+      currency: 'EUR',
+      poCustomer: 'Cust for Quotation',
+      item: 'ICHAUS/IC-HG30 QFN28-5X5',
+      poNumber: '112000104',
+      cost: 3.92,
+      quantity: 73,
+      creationDate: '2024-10-25',
+      noteToVendor: 'E/F/XIAO TIAN LI',
+      type: 'QUOTATION',
+      rowId: 1,
+      moq: 73
+    },
+    {
+      currency: 'EUR',
+      poCustomer: 'Cust for Quotation',
+      item: 'ICHAUS/IC-HG30 QFN28-5X5',
+      poNumber: '112000104',
+      cost: 6,
+      quantity: 1,
+      creationDate: '2023-04-10',
+      noteToVendor: 'E/F/XIAO TIAN LI',
+      type: 'QUOTATION',
+      rowId: 2,
+      moq: 73
+    },
+    {
+      currency: 'EUR',
+      poCustomer: 'Cust for Quotation',
+      item: 'ICHAUS/IC-HG30 QFN28-5X5',
+      poNumber: '112000104',
+      cost: 2.97,
+      quantity: 250000,
+      creationDate: '2022-12-15',
+      noteToVendor: 'E/F/XIAO TIAN LI',
+      type: 'QUOTATION',
+      rowId: 3,
+      moq: 73
+    },
+    {
+      currency: 'EUR',
+      poCustomer: 'Cust for Quotation',
+      item: 'ICHAUS/IC-HG30 QFN28-5X5',
+      poNumber: '112000104',
+      cost: 4.09,
+      quantity: 10000,
+      creationDate: '2022-12-15',
+      noteToVendor: 'E/F/XIAO TIAN LI',
+      type: 'QUOTATION',
+      rowId: 4,
+      moq: 73
+    },
+    {
+      currency: 'EUR',
+      poCustomer: 'Cust for Quotation',
+      item: 'ICHAUS/IC-HG30 QFN28-5X5',
+      poNumber: '112000104',
+      cost: 3.12,
+      quantity: 100000,
+      creationDate: '2022-12-15',
+      noteToVendor: 'E/F/XIAO TIAN LI',
+      type: 'QUOTATION',
+      rowId: 5,
+      moq: 73
+    },
+    {
+      currency: 'EUR',
+      poCustomer: 'Cust for Quotation',
+      item: 'ICHAUS/IC-HG30 QFN28-5X5',
+      poNumber: '112000104',
+      cost: 3.83,
+      quantity: 20000,
+      creationDate: '2022-12-15',
+      noteToVendor: 'E/F/XIAO TIAN LI',
+      type: 'QUOTATION',
+      rowId: 6,
+      moq: 73
+    },
+    {
+      currency: 'EUR',
+      poCustomer: 'Cust for Quotation',
+      item: 'ICHAUS/IC-HG30 QFN28-5X5',
+      poNumber: '112000104',
+      cost: 3.39,
+      quantity: 50000,
+      creationDate: '2022-12-15',
+      noteToVendor: 'E/F/XIAO TIAN LI',
+      type: 'QUOTATION',
+      rowId: 7,
+      moq: 73
+    },
+    {
+      currency: 'EUR',
+      poCustomer: '合肥美亚光电技术股份有限公司',
+      item: 'ICHAUS/IC-HG30 QFN28-5X5',
+      poNumber: '339080480',
+      cost: 3.92,
+      quantity: 73,
+      creationDate: '2026-04-02',
+      noteToVendor: null,
+      type: 'PO',
+      rowId: 8,
+      moq: null
+    },
+    {
+      currency: 'EUR',
+      poCustomer: '梅卡曼德（雄安）机器人科技股份有限公司',
+      item: 'ICHAUS/IC-HG30 QFN28-5X5',
+      poNumber: '349021829',
+      cost: 3.92,
+      quantity: 1460,
+      creationDate: '2026-04-01',
+      noteToVendor: null,
+      type: 'PO',
+      rowId: 9,
+      moq: null
+    },
+    {
+      currency: 'EUR',
+      poCustomer: '合肥瑞识智能科技有限公司',
+      item: 'ICHAUS/IC-HG30 QFN28-5X5',
+      poNumber: '339080258',
+      cost: 3.92,
+      quantity: 73,
+      creationDate: '2026-03-25',
+      noteToVendor: null,
+      type: 'PO',
+      rowId: 10,
+      moq: null
+    }
+  ]
+  tableRef.value?.loadData(data)
 }
 
 function handleSubmit() {
-  emits('submit', selectList.value)
+  const list = selectList.value
+    .sort((a, b) => {
+      const am = a.moq == null ? -Infinity : Number(a.moq)
+      const bm = b.moq == null ? -Infinity : Number(b.moq)
+      return bm - am
+    })
+    .map((item: any) => ({
+      moq: item.moq,
+      quantity: item.quantity,
+      cost: item.cost,
+      currency: item.currency
+    }))
+  emits('submit', { index: index.value, list })
+  showDialog.value = false
 }
 
 defineExpose({ open })
 </script>
 
 <template>
-  <el-dialog v-model="showDialog" title="歷史價格 historical price" class="big">
+  <el-dialog v-model="showDialog" title="歷史價格 historical price" class="big" append-to-body>
     <VxeGrid ref="tableRef" v-bind="tableConfig" v-on="tableEvent">
       <template #toolbar_buttons>
         <el-form :inline="true" label-position="top" class="historical-price-filter-form">
@@ -65,7 +230,7 @@ defineExpose({ open })
             <el-input v-model="searchData.series" clearable />
           </el-form-item>
           <el-form-item label="日期范圍 Date Range" prop="date_range">
-            <el-date-picker v-model="searchData.date_range" type="daterange" format="YYYY-MM-DD" />
+            <el-date-picker v-model="searchData.date_range" type="daterange" format="YYYY-MM-DD" value-format="x" />
           </el-form-item>
           <el-form-item label="類型 Type" prop="type">
             <el-select v-model="searchData.type" placeholder="請選擇類型" clearable>
--- packages/workflow/components/global/formWidget/quotationApproval/pcReview.vue
+++ packages/workflow/components/global/formWidget/quotationApproval/pcReview.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { Delete, Plus } from '@element-plus/icons-vue'
+import { clientApi, newClientApi } from 'api'
+import { Delete } from '@element-plus/icons-vue'
+import { v7 as uuidv7 } from 'uuid'
 
 const { disabled, formData, options } = defineProps<{
   disabled: boolean
@@ -41,10 +43,10 @@ type TargetPriceItem = {
   target_price: number
   unit_price_no_tax: number
   unit_cost: number
-  margin: number
+  margin: string
   customer_final_price: number
   sales_price: number
-  status: string
+  status: 'A' | 'D'
 }
 
 const formModel = ref<{
@@ -80,7 +82,7 @@ const formModel = ref<{
 })
 const data = toRef(formModel.value, 'infoList')
 const part_numberOptions = ref([])
-const showDetails = ref<boolean>(false)
+const showDetails = ref<boolean[]>([])
 const costCurrencyOptions = ref([
   { label: 'CNY', value: 'CNY' },
   { label: 'EUR', value: 'EUR' },
@@ -88,37 +90,167 @@ const costCurrencyOptions = ref([
   { label: 'HKD', value: 'HKD' },
   { label: 'USD', value: 'USD' }
 ])
+const exchangeRateList = ref<any[]>([])
+
+async function getExchangeRateList() {
+  exchangeRateList.value = await getDbData('aca40000-75dc-11f1-850d-35881bc838c2')
+}
+async function getDbData(tableId: string, conditions?: any[]) {
+  // Get Filed Mapping
+  const filedData: any = await newClientApi
+    .getDocpalMasterTableUserConfig({
+      tableId: tableId,
+      userId: 'master',
+      type: 'detail'
+    })
+    .then((res) => res.data)
+  const filedMapping: any = {}
+  filedData.tableFields.forEach((item: any) => {
+    filedMapping[item.field_name as string] = item.field_name_alias
+  })
+
+  const param = {
+    tableId: tableId,
+    conditions,
+    columns: [
+      {
+        name: '*'
+      }
+    ],
+    pagination: {
+      pageSize: 1000,
+      pageNum: 0
+    }
+  }
+
+  // Get BD Data
+  const dbData = await clientApi.instance.post('/apis/v1/dynamic-actions', param).then((res: any) => res.data.data)
+
+  // 匹配數據
+  return dbData.map((row: any) => {
+    const out = {}
+    for (const [fromKey, toKey] of Object.entries(filedMapping)) {
+      if (fromKey in row) out[toKey] = row[fromKey]
+    }
+    return out
+  })
+}
 
 function handleSampleInfoRemove(index: number) {
   data.value.splice(index, 1)
 }
 
 const historyPriceRef = ref()
-function openDialog(item: any) {
+function openDialog(index: number, item: any) {
+  item.index = index
   item.currency = formData.currency
   item.org_id = 1
   historyPriceRef.value.open(item)
 }
+
+function handelCostCurrency(item: any) {
+  const cost_currency = item.cost_currency
+  const currency = formData.currency
+  if (!cost_currency || !currency) return
+
+  const find = exchangeRateList.value.find((item: any) => item.base_currency === cost_currency && item.target_currency === currency)
+  item.exchange_rate = find.exchange_rate
+}
+
 function handleHistoryPriceSubmit(data: any) {
-  console.log(data)
+  const item = formModel.value.infoList[data.index]
+  const list: any = data.list || []
+
+  let newTarget_price_list: any[] = []
+
+  if (list.length >= item.target_price_list?.length) {
+    newTarget_price_list = list.map((newItem: any, index: number) => {
+      const priceItem = item.target_price_list[index]
+      let newPriceItem
+      if (!!priceItem) {
+        newPriceItem = {
+          ...priceItem,
+          moq: newItem.moq,
+          unit_cost: newItem.unit_cost
+        }
+      } else {
+        newPriceItem = {
+          sample_id: uuidv7(),
+          tier_number: index + 1,
+          moq: newItem.moq,
+          target_price: newItem.target_price,
+          unit_cost: newItem.unit_cost,
+          customer_final_price: 0,
+          sales_price: 0,
+          status: 'A',
+          unit_price_no_tax: newItem.unit_price_no_tax,
+          margin: ''
+        } as TargetPriceItem
+      }
+      calculateMargin(item.exchange_rate, newPriceItem)
+      return newPriceItem
+    })
+  } else {
+  }
+
+  console.log(123, newTarget_price_list)
 }
 
 function handleTargetPriceItemRemove(index: number, targetPriceIndex: number) {
   data.value[index].target_price_list.splice(targetPriceIndex, 1)
 }
 
-// (price - unitCost × exchangeRate × markup) / ( unitCost × exchangeRate × markup) × 100
-function calculateMargin(item: TargetPriceItem) {
-  // const { unit_price_no_tax, unit_cost, exchange_rate, moq } = item
-  // item.margin = ((unit_price_no_tax - unit_cost * exchange_rate * moq) / (unit_cost * exchange_rate * moq)) * 100
+// (target_price - unit_cost × exchange_rate × markup_rate) / ( unit_cost × exchange_rate × markup_rate) × 100
+function calculateMargin(exchange_rate: number, item: TargetPriceItem) {
+  const targetPrice = Number(item.target_price)
+  const unitPriceNoTax = Number(item.unit_price_no_tax)
+  const unitCost = Number(item.unit_cost)
+  // TODO: 需要管理層去確認的數據來源。Hank 反饋的暫時寫死
+  const markup_rate = 1.1
+
+  if (!unitPriceNoTax || !unitCost) {
+    item.margin = 0
+    return
+  }
+  item.margin = ((targetPrice - unitCost * exchange_rate * markup_rate) / (unitCost * exchange_rate * markup_rate)) * 100
+}
+
+function init() {
+  formModel.value = {
+    brand: formData.brand,
+    infoList: formData.sample_info_list
+  }
 }
 
 async function getFormData(needValidation = true) {
-  const result = { sample_info_list: formModel.list }
+  const result = { set_sample_list: formModel.value.infoList }
   if (!needValidation) return result
   await formRef.value?.validate()
   return result
 }
+
+onMounted(async () => {
+  await getExchangeRateList()
+})
+
+watch(
+  () => formModel.value.infoList.length,
+  (length) => {
+    showDetails.value = Array.from({ length }, (_, index) => showDetails.value[index] ?? false)
+  },
+  { immediate: true }
+)
+
+watch(
+  () => formData.set_sample_list,
+  (value) => {
+    if (!!value && value.length > 0) {
+      init()
+    }
+  },
+  { immediate: true, deep: true }
+)
+
 defineExpose({ getFormData })
 </script>
 
@@ -137,7 +269,9 @@ defineExpose({ getFormData })
         <div class="info-item-card__header">
           <span class="info-item-card__index">{{ index + 1 }}.</span>
           <div class="info-item-card__actions">
-            <el-button type="primary" v-model="showDetails" @click="showDetails = !showDetails">{{ showDetails ? '更多詳情' : '隱藏詳情' }}</el-button>
+            <el-button type="primary" @click="showDetails[index] = !showDetails[index]">
+              {{ showDetails[index] ? '隱藏詳情' : '更多詳情' }}
+            </el-button>
             <!--            <el-button :icon="Delete" type="danger" @click="handleSampleInfoRemove(index)" />-->
           </div>
         </div>
@@ -166,7 +300,7 @@ defineExpose({ getFormData })
 
           <el-col :span="8">
             <el-form-item label="貨幣 Cost Currency">
-              <el-select v-model="item.cost_currency" class="full-width-input" clearable>
+              <el-select v-model="item.cost_currency" class="full-width-input" clearable @change="handelCostCurrency(item)">
                 <el-option v-for="item in costCurrencyOptions" :key="item.value" :label="item.label" :value="item.value" />
               </el-select>
             </el-form-item>
@@ -185,14 +319,14 @@ defineExpose({ getFormData })
           </el-col>
 
           <el-col :span="8">
-            <el-form-item label="匯率 Unit Cost">
+            <el-form-item label="成本 Unit Cost">
               <el-input-number v-model="item.unit_cost" controls-position="right" :min="0.00001" :step="0.00001" step-strictly />
             </el-form-item>
             <el-form-item label="單價(未稅) Unit Price(No Tax)">
               <el-input-number v-model="item.unit_price_no_tax" controls-position="right" :min="0.00001" :step="0.00001" step-strictly />
             </el-form-item>
-            <el-form-item label="成本 Exchange Rate">
-              <el-input-number v-model="item.exchange_rate" controls-position="right" step-strictly disabled />
+            <el-form-item label="匯率 Exchange Rate">
+              <el-input-number v-model="item.exchange_rate" controls-position="right" :min="0.00001" :step="0.00001" step-strictly disabled />
             </el-form-item>
             <!--            <el-form-item label="狀態 Status">-->
             <!--              <el-select v-model="item.status" class="full-width-input">-->
@@ -206,7 +340,7 @@ defineExpose({ getFormData })
             <div class="targetPrice-item-card">
               <div class="targetPrice-item-card__header">
                 <span>設定不同數量檔位的目標價。 Higher MOQ → lower target price.</span>
-                <el-button type="primary" @click="openDialog(item)">檢索歷史價格 Retrieve historical prices</el-button>
+                <el-button type="primary" @click="openDialog(index, item)">檢索歷史價格 Retrieve historical prices</el-button>
               </div>
               <el-divider />
               <el-row class="targetPrice-item-card__table-header">
@@ -220,66 +354,68 @@ defineExpose({ getFormData })
               </el-row>
               <div class="targetPrice-item-card__body" :class="{ 'targetPrice-item-card__body--scrollable': item.target_price_list.length > 5 }">
                 <el-row v-for="(targetPriceItem, targetPriceIndex) in item.target_price_list" :key="targetPriceIndex">
-                  <el-col :span="2">第{{ targetPriceIndex + 1 }}檔 / T{{ targetPriceIndex + 1 }}</el-col>
-                  <el-col :span="4">
-                    <el-input-number
-                      style="width: 90%"
-                      v-model="targetPriceItem.moq"
-                      controls-position="right"
-                      :min="1"
-                      :step="1"
-                      step-strictly
-                      @change="calculateMargin(targetPriceItem)"
-                    />
-                  </el-col>
-                  <el-col :span="4">
-                    <el-input-number
-                      style="width: 90%"
-                      v-model="targetPriceItem.target_price"
-                      controls-position="right"
-                      :min="0.00001"
-                      :step="0.00001"
-                      step-strictly
-                      disabled
-                      @change="calculateMargin(targetPriceItem)"
-                    />
-                  </el-col>
-                  <el-col :span="5">
-                    <el-input-number
-                      style="width: 90%"
-                      v-model="targetPriceItem.unit_price_no_tax"
-                      controls-position="right"
-                      :min="0.00001"
-                      :step="0.00001"
-                      step-strictly
-                      @change="calculateMargin(targetPriceItem)"
-                    />
-                  </el-col>
-                  <el-col :span="4">
-                    <el-input-number
-                      style="width: 90%"
-                      v-model="targetPriceItem.unit_cost"
-                      controls-position="right"
-                      :min="0.00001"
-                      :step="0.00001"
-                      step-strictly
-                      @change="calculateMargin(targetPriceItem)"
-                    />
-                  </el-col>
-                  <el-col :span="3">
-                    <el-input style="width: 90%" v-model="targetPriceItem.margin" disabled />
-                  </el-col>
-                  <el-col :span="2">
-                    <div class="targetPrice-item-card__actions" v-if="targetPriceIndex !== 0">
-                      <el-button :icon="Delete" type="danger" @click="handleTargetPriceItemRemove(index, targetPriceIndex)" />
-                    </div>
-                  </el-col>
+                  <template v-if="targetPriceItem.status !== 'D'">
+                    <el-col :span="2">第{{ targetPriceIndex + 1 }}檔 / T{{ targetPriceIndex + 1 }}</el-col>
+                    <el-col :span="4">
+                      <el-input-number
+                        style="width: 90%"
+                        v-model="targetPriceItem.moq"
+                        controls-position="right"
+                        :min="1"
+                        :step="1"
+                        step-strictly
+                        @change="calculateMargin(item.exchange_rate, targetPriceItem)"
+                      />
+                    </el-col>
+                    <el-col :span="4">
+                      <el-input-number
+                        style="width: 90%"
+                        v-model="targetPriceItem.target_price"
+                        controls-position="right"
+                        :min="0.00001"
+                        :step="0.00001"
+                        step-strictly
+                        disabled
+                        @change="calculateMargin(item.exchange_rate, targetPriceItem)"
+                      />
+                    </el-col>
+                    <el-col :span="5">
+                      <el-input-number
+                        style="width: 90%"
+                        v-model="targetPriceItem.unit_price_no_tax"
+                        controls-position="right"
+                        :min="0.00001"
+                        :step="0.00001"
+                        step-strictly
+                        @change="calculateMargin(item.exchange_rate, targetPriceItem)"
+                      />
+                    </el-col>
+                    <el-col :span="4">
+                      <el-input-number
+                        style="width: 90%"
+                        v-model="targetPriceItem.unit_cost"
+                        controls-position="right"
+                        :min="0.00001"
+                        :step="0.00001"
+                        step-strictly
+                        @change="calculateMargin(item.exchange_rate, targetPriceItem)"
+                      />
+                    </el-col>
+                    <el-col :span="3">
+                      <el-input style="width: 90%" v-model="targetPriceItem.margin" disabled />
+                    </el-col>
+                    <el-col :span="2">
+                      <div class="targetPrice-item-card__actions" v-if="targetPriceIndex !== 0">
+                        <el-button :icon="Delete" type="danger" @click="handleTargetPriceItemRemove(index, targetPriceIndex)" />
+                      </div>
+                    </el-col>
+                  </template>
                 </el-row>
               </div>
             </div>
           </el-col>
 
-          <template v-if="!showDetails">
+          <template v-if="showDetails[index]">
             <el-divider />
             <el-col :span="6">
               <el-form-item label="月用量 Monthly Quantity" prop="monthly_quantity">
