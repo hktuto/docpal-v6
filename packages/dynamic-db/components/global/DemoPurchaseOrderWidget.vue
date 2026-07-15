b28b97652f3b3fe796eda83de84715d7307f3287
Sean Tsang <9394wing@gmail.com>
2026-07-15 16:22:20 +0800

    update filter

--- packages/dynamic-db/components/global/DemoPurchaseOrderWidget.vue
+++ packages/dynamic-db/components/global/DemoPurchaseOrderWidget.vue
@@ -117,8 +117,7 @@ const year = useDemoYear()
 
 const filteredRows = computed(() => {
   const rows = applyDemoFilters(rawRows.value, filterDefs, filterState.value)
-  return (brands.value.includes(ALL_BRANDS) ? rows : rows.filter((r) => brands.value.includes(r.brand)))
-    .filter((r) => inDemoYear(r.orderDate, year.value))
+  return rows
 })
 
 const treeData = computed<DemoTreeNode[]>(() =>
