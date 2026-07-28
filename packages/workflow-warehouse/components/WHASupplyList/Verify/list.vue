99f20b04665186e743d470c30617d91a2a2e4efb
OnlY <oncwnuFEkWf95h0cczASlBOD8O60@git.weixin.qq.com>
2026-07-28 13:19:33 +0800

    [warehouse]Adjust Total_Ctn

--- packages/workflow-warehouse/components/WHASupplyList/Verify/list.vue
+++ packages/workflow-warehouse/components/WHASupplyList/Verify/list.vue
@@ -102,9 +102,9 @@ onMounted(() => {
     >
       <div class="invoice-card-body">
         <div class="invoice-card-name">{{ item[SGLA.Name] }}</div>
-        <div v-if="item.total_qty || item[SGLA.Total_Ctn]" class="invoice-card-meta">
+        <div class="invoice-card-meta">
           <template v-if="item.total_qty"> {{ item[SGLA.Total_Ctn] || 0 }} cartons </template>
-          <template v-if="item.total_qty && item[SGLA.Total_Ctn]"> • </template>
+          •
           <span v-if="item.total_qty" class="invoice-card-meta-qty">{{ item.total_qty || 0 }} units</span>
         </div>
       </div>
