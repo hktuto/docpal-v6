2a8122e0a1586c69964efca9c458e870cfe7b810
Sean Tsang <9394wing@gmail.com>
2026-07-28 10:42:49 +0800

    remove old package

--- apps/client/app.config.ts
+++ apps/client/app.config.ts
@@ -77,9 +77,9 @@ export default defineAppConfig({
       ]
     },
 
-    {
-      name: 'client-master-table'
-    },
+    // {
+    //   name: 'client-master-table'
+    // },
     // {
     //   name: 'client-dashboard'
     // },
@@ -162,9 +162,9 @@ export default defineAppConfig({
         {
           name: 'admin-work-panel'
         },
-        {
-          name: 'admin-master-table'
-        }
+        // {
+        //   name: 'admin-master-table'
+        // }
       ]
     },
     {
@@ -275,9 +275,9 @@ export default defineAppConfig({
       name: 'admin-import-jobs'
     },
 
-    {
-      name: 'admin-azure'
-    },
+    // {
+    //   name: 'admin-azure'
+    // },
 
     {
       name: 'admin-easy-form'
--- pages/client-master-table/__test__/mock/api.ts
+++
@@ -1,8 +0,0 @@
-import { vi } from 'vitest'
-
-export const clientApi = {
-  api: {
-    getMasterTablesFindAllByUser: vi.fn(() => Promise.resolve(true)),
-    getMasterTablesId: vi.fn(() => Promise.resolve(true)),
-  }
-}
