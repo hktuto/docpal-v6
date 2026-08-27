df34c7e10c7e94d965ae545b05a5000df07629ea
OnlY <oncwnuFEkWf95h0cczASlBOD8O60@git.weixin.qq.com>
2026-08-27 14:16:33 +0800

    [GIT] Structural adjustment

--- packages/workflow-warehouse/plugins/warehouse.ts
+++ packages/workflow-warehouse/plugins/warehouse.ts
@@ -5,6 +5,4 @@ export default defineNuxtPlugin((nuxtApp) => {
   const { formRenderSlots, formStartHandle } = useWorkflow()
   formRenderSlots.value.push({ name: 'WHASupplyListVerify', component: WHASupplyListVerify })
   formRenderSlots.value.push({ name: 'WHInvoiceSupplyList', component: WHInvoiceSupplyList })
-  // 兼容旧表单插槽名
-  formRenderSlots.value.push({ name: 'WHASupplyListInvoiceVerify', component: WHInvoiceSupplyList })
 })
