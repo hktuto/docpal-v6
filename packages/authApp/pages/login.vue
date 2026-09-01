81a5b958239e8d1a2cceb9efc17a4fa6c929e16c
OnlY0226 <1299962367@qq.com>
2026-09-01 08:59:57 +0800

    DPL-9707 【ppt-4】Login Page: ENTER key does not submit login when credentials are saved by browser

--- packages/authApp/pages/login.vue
+++ packages/authApp/pages/login.vue
@@ -31,6 +31,7 @@ function resolveRedirectPath() {
 
 async function submit() {
   try {
+    if (!form.username || !form.password) return
     loading.value = true
     errorMessage.value = ''
     const result = await loginWithPassword(form.username, form.password)
@@ -38,11 +39,11 @@ async function submit() {
       errorMessage.value = result.message
       return
     }
-    form.username = ''
-    form.password = ''
     if (!result.passwordResetRequired) {
       await router.push(resolveRedirectPath())
     }
+    form.username = ''
+    form.password = ''
   } finally {
     loading.value = false
   }
@@ -72,8 +73,20 @@ async function initLoginPage() {
   }
 }
 
+function handleEnterKey(event: KeyboardEvent) {
+  if (event.key !== 'Enter') return
+  if (loading.value) return
+  event.preventDefault()
+  submit()
+}
+
 onMounted(() => {
   initLoginPage()
+  window.addEventListener('keydown', handleEnterKey)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', handleEnterKey)
 })
 </script>
 
@@ -86,7 +99,7 @@ onMounted(() => {
           <ElInput ref="usernameEl" v-model="form.username" type="text" />
         </ElFormItem>
         <ElFormItem label="Password" :rules="rules.password">
-          <ElInput v-model="form.password" type="password" @keyup.enter.native="submit" show-password />
+          <ElInput v-model="form.password" type="password" show-password />
         </ElFormItem>
         <ElFormItem>
           <ElAlert v-if="errorMessage" :title="errorMessage" type="error" />
